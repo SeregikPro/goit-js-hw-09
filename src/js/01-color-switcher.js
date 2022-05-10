66349892d271a5309e588d7b885e68d85a495250
Sergii Proiava <seregikpro@gmail.com>
2022-05-11 00:28:06 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const PROMT_DELAY = 1000;
+let timeoutId = null;
 
+const startClick = document.querySelector('[data-start]');
+const stopClick = document.querySelector('[data-stop]');
+
+startClick.addEventListener('click', onClick);
+stopClick.addEventListener('click', offClick);
+stopClick.disabled = true;
+
+function onClick() {
+    if (startClick) {
+        stopClick.disabled = true;
+    }
+    timeoutId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, PROMT_DELAY)
+}
+
+function offClick() {
+    if (stopClick) {
+        startClick.disabled = false;
+    }
+    clearInterval(timeoutId); 
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
